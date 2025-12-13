a1e7dc456f66a05edf26d2bc3f230e5164f4a2b8
*abhaysingh128* <*abhaysinghbisht2003@gmail.com*>
2025-12-13 23:40:23 +0530

    13dec practice

---
+++ STACK/remove_adjacent_duplicate.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+string removeDuplicates(string s) {
+        stack<char>st;
+        string ans;
+        for(int i=0;i<s.length();i++)
+        {
+         if(st.empty())
+         { 
+            st.push(s[i]);
+            continue;
+         }
+         if(s[i]==st.top())
+         {
+            st.pop();
+            continue;
+         }
+         st.push(s[i]);   
+        }
+        while(!st.empty())
+        {
+            char c=st.top();
+            st.pop();
+            ans.push_back(c);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+}
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<removeDuplicates(s)<<endl;
+    return 0;
+}
---
+++ STACK/valid-parentheses.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(' || s[i]=='{'||s[i]=='[')
+            {
+                    st.push(s[i]);
+                    continue;
+            }
+            if(st.empty()) return false;
+            if(s[i]==')' && st.top()=='(')
+            {
+                 st.pop();
+                 continue;
+            }
+            if(s[i]==']' && st.top()=='[')
+             {
+                 st.pop();
+                 continue;
+            }
+            if(s[i]=='}' && st.top()=='{') 
+             {
+                 st.pop();
+                 continue;
+            }
+            else return false;
+        }
+        if (st.empty()) return true;
+        return false;
+        
+    }
+int main()
+{
+    string s;
+    cout<<"Enter the parentheses string: ";
+    cin>>s;
+    if(isValid(s))
+        cout<<"The parentheses string is valid."<<endl;
+    else
+        cout<<"The parentheses string is not valid."<<endl;
+    return 0;
+}
